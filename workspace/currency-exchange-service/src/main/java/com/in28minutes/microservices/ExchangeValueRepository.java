49a9c203f0c40e8ca6f44a53c111abc124ef3607
Tony <tonysobreira@gmail.com>
2023-06-05 17:22:29 -0300

    update

---
+++ workspace/currency-exchange-service/src/main/java/com/in28minutes/microservices/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.in28minutes.microservices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+
+	ExchangeValue findByFromAndTo(String from, String to);
+
+}
