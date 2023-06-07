7cfc6c12cc80523b950fdf4e581fb0db9e047e0b
Tony <tonysobreira@gmail.com>
2023-06-07 17:26:55 -0300

    update

--- workspace/currency-conversion-service/src/main/java/com/in28minutes/microservices/CurrencyConversionServiceApplication.java
+++ workspace/currency-conversion-service/src/main/java/com/in28minutes/microservices/CurrencyConversionServiceApplication.java
@@ -2,7 +2,11 @@ package com.in28minutes.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.in28minutes.microservices")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
--- workspace/netflix-eureka-naming-server/src/main/java/com/in28minutes/microservices/NetflixEurekaNamingServerApplication.java
+++ workspace/netflix-eureka-naming-server/src/main/java/com/in28minutes/microservices/NetflixEurekaNamingServerApplication.java
@@ -2,14 +2,14 @@ package com.in28minutes.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
-public class CurrencyExchangeServiceApplication {
+public class NetflixEurekaNamingServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
 	}
 
 }
